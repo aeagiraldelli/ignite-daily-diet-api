e809a8b2223dc36d36595d44a5a30f25c6398cac
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2023-11-26 15:08:07 -0300

    feat: middleware to check if userId exists.

---
+++ src/middlewares/check-user-id-exists.ts
@@ -0,0 +1,8 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+
+export async function checkUserIdExists(request: FastifyRequest, reply: FastifyReply) {
+  const userId = request.cookies.userId;
+  if (!userId) {
+    return reply.status(401).send();
+  }
+}
