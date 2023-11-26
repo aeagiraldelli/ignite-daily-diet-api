4167ac9f540c8ad07e77ea257f505be22d764e8c
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2023-11-26 14:38:59 -0300

    chore: database migrations.

---
+++ db/migrations/20231126172035_create-meals-table.ts
@@ -0,0 +1,19 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('meals', (table) => {
+    table.uuid('id').primary();
+    table.text('name').notNullable();
+    table.text('description').nullable();
+    table.boolean('planned').notNullable();
+    table.text('user_id').references('id').inTable('users');
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
+    table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable();
+  });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('meals');
+}
+
---
+++ src/database/index.ts
@@ -0,0 +1,18 @@
+import { knex as setup, Knex } from 'knex';
+import { env } from '../env';
+
+export const config: Knex.Config = {
+  client: env.DATABASE_CLIENT,
+  connection: env.NODE_ENV === 'test'
+    ? {
+      filename: env.DATABASE_URL
+    }
+    : env.DATABASE_URL,
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+};
+
+export const knex = setup(config);
