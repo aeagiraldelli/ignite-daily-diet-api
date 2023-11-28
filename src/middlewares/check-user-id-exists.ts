5cbc23f0314ba44420e01d3dd8e75f664c046798
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2023-11-27 21:03:14 -0300

    feat: meals routes.

--- src/middlewares/check-user-id-exists.ts
+++ src/middlewares/check-user-id-exists.ts
@@ -1,5 +1,6 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { z } from 'zod';
+import { knex } from '../database';
 
 export async function checkUserIdExists(request: FastifyRequest, reply: FastifyReply) {
   const idSchema = z.object({
@@ -7,8 +8,12 @@ export async function checkUserIdExists(request: FastifyRequest, reply: FastifyR
   });
 
   const { userId } = idSchema.parse(request.cookies);
-
   if (!userId) {
     return reply.status(401).send();
+  }
+
+  const user = await knex('users').where({ id: userId }).first();
+  if (!user) {
+    return reply.status(404).send();
   }
 }
--- src/routes/meals-routes.ts
+++ src/routes/meals-routes.ts
@@ -6,13 +6,11 @@ import { z } from 'zod';
 import { checkUserIdExists } from '../middlewares/check-user-id-exists';
 import { knex } from '../database';
 
-export function mealsRoutes(app: FastifyInstance) {
+export async function mealsRoutes(app: FastifyInstance) {
   app.get('/', { preHandler: [checkUserIdExists] }, async (request, reply) => {
     const { userId } = request.cookies;
     const meals = await knex('meals').where('user_id', userId);
-    reply.code(200).send({
-      meals: meals
-    });
+    reply.code(200).send({ meals });
   });
 
   app.get('/:id', { preHandler: [checkUserIdExists] }, async (request, reply) => {
@@ -24,8 +22,7 @@ export function mealsRoutes(app: FastifyInstance) {
     const { userId } = request.cookies;
 
     const meal = await knex('meals')
-      .where('user_id', userId)
-      .where('id', id)
+      .where({ user_id: userId, id })
       .first();
 
     if (meal) {
@@ -55,5 +52,88 @@ export function mealsRoutes(app: FastifyInstance) {
     });
 
     return reply.code(201).send();
+  });
+
+  app.put('/:id', { preHandler: [checkUserIdExists] }, async (request, reply) => {
+    const paramsSquema = z.object({
+      id: z.string().uuid(),
+    });
+    const bodySchema = z.object({
+      name: z.string(),
+      description: z.string().optional(),
+      planned: z.boolean()
+    });
+
+    const { userId } = request.cookies;
+    const { id } = paramsSquema.parse(request.params);
+    const { name, description, planned } = bodySchema.parse(request.body);
+
+    const registeredMeal = await knex('meals').where({ user_id: userId, id }).first();
+    if (!registeredMeal) {
+      return reply.code(404).send({ error: 'Meal not found.' });
+    }
+
+    await knex('meals').update({
+      name,
+      description,
+      planned,
+      updated_at: knex.fn.now(),
+    }).where({ id });
+
+    return reply.code(200).send();
+  });
+
+  app.delete('/:id', { preHandler: [checkUserIdExists] }, async (request, reply) => {
+    const { userId } = request.cookies;
+    const paramsSquema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = paramsSquema.parse(request.params);
+
+    const registeredMeal = await knex('meals').where({ user_id: userId, id }).first();
+    if (!registeredMeal) {
+      return reply.code(404).send();
+    }
+
+    await knex('meals').where({ user_id: userId, id }).first().delete();
+
+    return reply.code(200).send();
+  });
+
+  app.get('/metrics', { preHandler: [checkUserIdExists] }, async (request, reply) => {
+    const { userId } = request.cookies;
+    const numOfMeals = await knex('meals').where('user_id', userId);
+    let numPlannedMeals = 0;
+    let bestSequenceCounter = 0;
+    let bestSequence = 0;
+
+    numOfMeals.forEach((meal) => {
+      if (meal.planned) {
+        numPlannedMeals += 1;
+        bestSequenceCounter += 1;
+      }
+      else {
+        if (bestSequenceCounter > bestSequence) {
+          bestSequence = bestSequenceCounter;
+        }
+
+        bestSequenceCounter = 0;
+      }
+    });
+
+    if (bestSequenceCounter > bestSequence) {
+      bestSequence = bestSequenceCounter;
+    }
+
+    const data = {
+      userId,
+      totalOfMeals: numOfMeals.length,
+      totalPlannedMeals: numPlannedMeals,
+      totalUnplannedMeals: numOfMeals.length - numPlannedMeals,
+      bestSequence
+    };
+
+    return reply.code(200).send(data);
   });
 }
